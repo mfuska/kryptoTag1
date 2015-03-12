452efba74bcb894bf8c897895c0a2a243636c974
mfuska <mfuska@gmx.at>
2015-03-12 12:59:29 +0100

    change ECC new EccMain file

--- ECC/ECC.java
+++ ECC/ECC.java
@@ -1,112 +1,102 @@
+package ECC;
+
 import java.math.BigInteger;
-import java.security.spec.ECFieldFp;
-import java.security.spec.EllipticCurve;
 
 /**
  * Created by mike on 06.03.15.
  */
 public class ECC {
-    /*
+
+    private static BigInteger TWO = new BigInteger("2");
+    protected static BigInteger DREI = new BigInteger("3");
+
     private BigInteger a;
     private BigInteger b;
     private BigInteger p;
-    private EllipticCurve curve;
 
-    private ECC(EllipticCurve curve, BigInteger a, BigInteger b, BigInteger p) {
-        this.curve = curve;
+    public ECC(BigInteger a, BigInteger b, BigInteger p) {
         this.a = a;
         this.b = b;
         this.p = p;
     }
-     */
 
-    public static class Point {
+    protected Point newPoint(BigInteger x, BigInteger y) {
+        return new Point(x,y);
+    }
+
+    protected class Point {
 
         private BigInteger x;
         private BigInteger y;
 
-        public Point(BigInteger x, BigInteger y) {
+        protected Point() {
+            this.x = BigInteger.ZERO;
+            this.y = BigInteger.ZERO;
+            System.out.println("Point x:" + x.toString(16) + " y:" + y.toString(16));
+        }
+        protected Point(BigInteger x, BigInteger y) {
             this.x = x;
             this.y = y;
-            System.out.println("Point x:" + x + " y:" + y);
+            System.out.println("Point x:" + x.toString(16) + " y:" + y.toString(16));
         }
 
-        private Point add(Point q) {
-            //x3 = (( ( (y2 - y1) / (x2 - x1) )^2 ) - x1 -x2 ) mod p
-            //y3 = (( ( (y2 - y1) / (x2 - x1) ) * ( x1 - x3 ) ) - y1 ) mod p
-
+        protected Point add(Point q) {
             // if (p1 != p2) lamda = (y2 - y1) / (x2 - x1)
             // else lamda = (3 * x1 * x1 + a)/ ( 2 * y1 )
 
-            System.out.println("this.y:" + this.y );
-            System.out.println("q.y:" + q.y );
-            System.out.println("this.x:" + this.x );
-            System.out.println("q.x:" + q.x );
-            System.out.println("q.y - y1:" + q.y.subtract(this.y) );
-            System.out.println("q.x - x1:" + q.x.subtract(this.x) );
+            if (this.x.equals(q.x) && this.y.equals(q.y)) {
+                return this.doubled();
+            } else {
+                //x3 = (( ( (y2 - y1) / (x2 - x1) )^2 ) - x1 -x2 ) mod p
+                //y3 = (( ( (y2 - y1) / (x2 - x1) ) * ( x1 - x3 ) ) - y1 ) mod p
 
-            // lamda = (y2 - y1) * (x2 - x1) ^ (-1)
-            // this^(-1) mod m
-            // modInverse(m)
-            BigInteger lamda = ((q.y.subtract(this.y).modInverse(q.x.subtract(this.x))));
-            //BigInteger lamda = ((q.x.subtract(this.x))).modInverse(q.x.subtract(this.x));
+                // lamda =  ( (y2 -y1) / (x2 -x1) )
+                // lamda = (q.y - this.y) *  (q.x - this.x).modInverse(p)
+                BigInteger lamda = (q.y.subtract(this.y).multiply(q.x.subtract(this.x).modInverse(ECC.this.p)));
 
-            //x3 = ( ( (y2 - y1) / (x2 - x1) )^2 ) - x1 -x2
-            //x3 = ( lamda * lamda ) - x1 -x2
-            BigInteger tmp = lamda.multiply(lamda);
-            BigInteger qx = tmp.subtract(this.x).subtract(q.x);
+                //x3 = (( ( (y2 - y1) / (x2 - x1) )^2 ) - x1 -x2 ) mod p
+                //x3 = ( lamda * lamda ) - x1 -x2
+                BigInteger qx = (lamda.pow(2).subtract(this.x).subtract(q.x)).mod(ECC.this.p);
+
+                //y3 = ( ( (y2 - y1) / (x2 - x1) ) * ( x1 - x3 ) ) - y1
+                //y3 = ( ( lamda ) * ( x1 - x3) ) - y1
+                BigInteger qy = (lamda.multiply(this.x.subtract(qx)).subtract(this.y)).mod(ECC.this.p);
+
+                return new Point(qx, qy);
+            }
+        }
+        protected Point doubled() {
+
+            //lamda =  (( (3 * px * px ) + this.a )  / ( 2 * py )) % p;
+            BigInteger lamda = ( this.x.pow(2).multiply( ECC.DREI ) ).add(ECC.this.a).multiply(this.y.multiply(ECC.TWO).modInverse(ECC.this.p));
+
+            //x3 = ( lamda ^2 ) - x1 -x2
+            BigInteger qx = ( lamda.pow(2).subtract(this.x.multiply( ECC.TWO ) ) ).mod(ECC.this.p);
 
             //y3 = ( ( (y2 - y1) / (x2 - x1) ) * ( x1 - x3 ) ) - y1
-            //y3 = ( ( lamda ) * ( x1 - x3) ) - y1
-            tmp = this.x.subtract(qx);
-            BigInteger qy = lamda.multiply(tmp).subtract(this.y);
+            BigInteger qy = (( lamda.multiply(this.x.subtract(qx)) ).subtract(this.y)).mod(ECC.this.p);
 
-            return new Point(qx, qy);
+            return new Point(qx,qy);
         }
+        protected Point scalar(BigInteger n) {
 
-        public BigInteger getY() {
-            return this.y;
+            Point r = new Point();
+            Point p = new Point(this.x, this.y);
+
+            for (int i = 0; i < n.bitLength(); i++) {
+                if (n.testBit(i)) {
+                    r = this.add(r);
+                }
+                p = p.doubled();
+            }
+            return r;
         }
-
-        public BigInteger getX() {
-            return this.x;
+        protected Boolean verify() {
+            // y ^ 2 = (x^3 + ax + b) mod p
+            System.out.println(this.y.pow(2).mod(ECC.this.p)) ;
+            System.out.println( ( this.x.pow(3).add( ECC.this.a.multiply(this.x) ).add( ECC.this.b ) ).mod( ECC.this.p) );
+            return true;
         }
-    }
-
-    /* private Point multiply(Point p1, int n) {
-         // if (p1 != p2) lamda = (y2 - y1) / (x2 - x1) mod p
-         // else lamda = ((3 * x1 * x1 + a)/ ( 2 * y1 ) ) mod p
-         double px = p1.x;
-         double py = p1.y;
-         double lamda;
-         double qx;
-         double qy;
-
-         //lamda = (( (3 * px * px ) + this.a ) % this.p) / (( 2 * py ) % this.p);
-         lamda =  (( (3 * px * px ) + this.a )  / ( 2 * py )) % this.p;
-         System.out.println("( 3 * " + px + " * " + px + " ) + " + this.a + " / ( 2 * " + py);
-         System.out.println("lamda:" + lamda);
-
-         //x3 = ( lamda ^2 ) - x1 -x2
-          qx = (lamda * lamda) - px - px;
-
-          //y3 = ( ( (y2 - y1) / (x2 - x1) ) * ( x1 - x3 ) ) - y1
-          qy = ( lamda * (px - qx) ) - py;
-          System.out.println("qx:" + qx + " qy:" + qy);
-          return new Point(qx,qy);
-     }
- */
-    public static void main(String[] args) {
-        // (y * y) = (x * x * x) + (a * x) + b
-        //EllipticCurve myCurve = new EllipticCurve(new ECFieldFp(new BigInteger("23")), new BigInteger("1"), new BigInteger("1"));
-        //ECC ecc = new ECC(myCurve, new BigInteger("1"), new BigInteger("1"), new BigInteger("23") );
-        Point a = new Point(new BigInteger("4"), new BigInteger("0"));
-        Point b = new Point(new BigInteger("3"), new BigInteger("10"));
-
-        Point c = a.add(b);
-
-        //Point d = ecc.multiply(b, 1);
-        //Point f = ecc.multiply(b, 2);
     }
 }
 
---
+++ ECC/EccMain.java
@@ -0,0 +1,26 @@
+package ECC;
+
+import java.math.BigInteger;
+
+/**
+ * Created by mike on 12.03.15.
+ */
+public class EccMain {
+
+    public static void main(String[] args) {
+        // (y * y) = (x * x * x) + (a * x) + b
+        // Setting for Curve P-224
+        BigInteger a = new BigInteger("-3");
+        BigInteger b = new BigInteger("b4050a850c04b3abf54132565044b0b7d7bfd8ba270b39432355ffb4", 16);
+        BigInteger p = new BigInteger("26959946667150639794667015087019630673557916260026308143510066298881");
+
+        ECC ecc = new ECC(a,b,p);
+
+        BigInteger gX = new BigInteger("b70e0cbd6bb4bf7f321390b94a03c1d356c21122343280d6115c1d21" ,16);
+        BigInteger gY = new BigInteger("bd376388b5f723fb4c22dfe6cd4375a05a07476444d5819985007e34", 16);
+
+        ECC.Point basisP = ecc.newPoint(gX, gY);
+        ECC.Point x = basisP.scalar(ecc.DREI);
+        System.out.println( x.verify());
+    }
+}
